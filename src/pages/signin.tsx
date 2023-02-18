54846b200a4cf8c8621aede343a4e62926b9b492
hasesho05 <midorinotanuki56@gmail.com>
2023-02-18 15:15:13 +0900

    メインページ実装

---
+++ src/pages/signin.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+
+const signin = () => {
+  return (
+    <Box>
+      <Typography>SignIn</Typography>
+      <Typography></Typography>
+    </Box>
+  );
+}
+
+export default signin;
